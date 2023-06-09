6f30e4ef91dd32e9842a62c4217bbb6a2bdf44fa
Pryanshu Gupta <86071448+pryang007@users.noreply.github.com>
2023-06-09 15:30:47 +0530

    Added solution - LeetHub

---
+++ Max Circular Subarray Sum - GFG/max-circular-subarray-sum.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // arr: input array
+    // num: size of array
+    //Function to find maximum circular subarray sum.
+    int circularSubarraySum(int arr[], int num){
+        
+        // your code here
+        int max_sum=INT_MIN;
+        int min_sum =0;
+        int maxsum=0;
+        int minsum=0;
+        int total_sum=0;
+        for(int i=0;i<num;i++) {
+            total_sum += arr[i];
+            maxsum = max(arr[i] + maxsum, arr[i]);
+            minsum = min(minsum + arr[i], arr[i]);
+            
+            max_sum = max(max_sum, maxsum);
+            min_sum = min(min_sum, minsum);
+        }
+        
+        if(min_sum == total_sum)
+            return max_sum;
+        
+        return max(max_sum, total_sum - min_sum);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+ {
+	int T;
+	
+	//testcases
+	cin>> T;
+	
+	while (T--)
+	{
+	    int num;
+	    
+	    //size of array
+	    cin>>num;
+	    int arr[num];
+	    
+	    //inserting elements
+	    for(int i = 0; i<num; i++)
+	        cin>>arr[i];
+	        
+	    Solution ob;
+	    //calling function
+	    cout << ob.circularSubarraySum(arr, num) << endl;
+	    
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
